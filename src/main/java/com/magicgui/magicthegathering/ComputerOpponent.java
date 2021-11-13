e278dfb4cb2fdb402a7a8c3ea7c9574a38855eec
cmm9339 <93937000+cmm9339@users.noreply.github.com>
2021-11-13 15:16:07 -0500

    Update ComputerOpponent.java

--- src/main/java/com/magicgui/magicthegathering/ComputerOpponent.java
+++ src/main/java/com/magicgui/magicthegathering/ComputerOpponent.java
@@ -1,4 +1,5 @@
-package magicgame.magicthegathering;
+package com.magicgui.magicthegathering;
+
 // Make the AI run behind the scenes
 // Grab a card
 // Play a card to the field
@@ -17,27 +18,18 @@ public class ComputerOpponent {
     ComputerOpponent.drawCard();
   }
 
-
-  // Attacking must consider how many cards in the hand(null), randomly select from the hand,
-  // and randomly place in the field.
-  // Starting out with 7 cards
-
-
- // while not loop place is null
+  
   public void computerPlaceCards() {
-    while (ComputerOpponent.getPlayField() != null){
+    {
       for (int x = 0; x <= 5; x++) {
         // iterate over playfield
         int PlaceHand = choice.nextInt((5 - 1) + 1) + 1;
         int PlaceField = choice.nextInt((5 - 1) + 1) + 1;
         // Check if spot on field is empty, if it is not, just increment to next spot
-        if (ComputerOpponent.getPlayField() != null) {
-          x = x + 1;
-        }
-        // If spot empty just place the card on the field
-        else {
+        if (ComputerOpponent.getPlayField() == null) {
           ComputerOpponent.placeCard(PlaceHand, PlaceField);
         }
+        // If spot empty just place the card on the field
       }
     }
   }
