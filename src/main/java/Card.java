c74c0d646e02cd7efc2d1580e9ec4fcc19ff0a7c
StripedMonkey <monkeyinastripedshirt@gmail.com>
2021-11-13 11:21:04 -0500

    Add getters, setters, and events that can be tracked

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,18 +1,19 @@
+import java.beans.PropertyChangeSupport;
 import java.io.File;
 import java.util.List;
+import java.beans.PropertyChangeEvent;
 
 public class Card {
-    public int baseHealth;
-    int attack;
-    int health;
-    int cost;
-    List<Effect> effects;
-    boolean isSpellCard;
-    Player ownerPlayer;
+    private boolean isSpellCard;
+    private int attack;
+    private int health;
+    private int cost;
 
+    PropertyChangeSupport eventHelper;
+    List<Effect> effects;
+    Player ownerPlayer;
     String description;
     String imagePath;
-
 
     Card(int attack, int health, int cost, List<Effect> effects, String description, String imagePath) {
         this.attack = attack;
@@ -21,6 +22,7 @@ public class Card {
         this.effects = effects;
         this.description = description;
         this.imagePath = imagePath;
+        eventHelper = new PropertyChangeSupport(this);
     }
 
     static List<Card> buildDeckFromFolder(File folder) {
@@ -31,8 +33,67 @@ public class Card {
         return buildDeckFromFolder(new File(folder));
     }
 
+    public boolean isSpellCard() {
+        return isSpellCard;
+    }
+
+    public void setSpellCard(boolean spellCard) {
+        isSpellCard = spellCard;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        int oldAttack = this.attack;
+        this.attack = attack;
+        eventHelper.firePropertyChange("AttackUpdate", oldAttack, this.attack);
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        int oldHealth = this.health;
+        this.health = health;
+        eventHelper.firePropertyChange("HealthUpdate", oldHealth, this.health);
+
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        int oldCost = this.health;
+        this.cost = cost;
+        eventHelper.firePropertyChange("CostUpdate", oldCost, this.cost);
+    }
+
+    public Player getOwnerPlayer() {
+        return ownerPlayer;
+    }
+
+    public void setOwnerPlayer(Player ownerPlayer) {
+        this.ownerPlayer = ownerPlayer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     String getImagePath() {
         return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
     }
 
     void removeEffect(Effect effect) {
@@ -56,7 +117,11 @@ public class Card {
     }
 
     public void damage(int damage) {
-        health -= damage;
+        setHealth(getHealth() - damage);
+    }
+
+    public void heal(int health) {
+        setHealth(getHealth() + health);
     }
 }
 
