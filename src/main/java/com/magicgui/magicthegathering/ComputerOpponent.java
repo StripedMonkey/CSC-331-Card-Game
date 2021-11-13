ab1746af98b3ade9528641c33e198aea370d3460
cmm9339 <93937000+cmm9339@users.noreply.github.com>
2021-11-13 14:55:09 -0500

    Update ComputerOpponent.java

--- src/main/java/com/magicgui/magicthegathering/ComputerOpponent.java
+++ src/main/java/com/magicgui/magicthegathering/ComputerOpponent.java
@@ -2,53 +2,41 @@ package magicgame.magicthegathering;
 // Make the AI run behind the scenes
 // Grab a card
 // Play a card to the field
-
 import java.util.Random;
-
+/** Computer Opponent **/
 public class ComputerOpponent {
   private Player ComputerOpponent;
   private Random choice;
   // Initialize the Player
-  ComputerOpponent(Player ComputerOpponent) { 
+  ComputerOpponent(Player ComputerOpponent) {
     this.ComputerOpponent = ComputerOpponent;
   }
-  
+
+
   public void computerDraw() {
     ComputerOpponent.drawCard();
   }
+
 
   // Attacking must consider how many cards in the hand(null), randomly select from the hand,
   // and randomly place in the field.
   // Starting out with 7 cards
 
-  public void computerPlaceCards() {
-    for (int x = 0; x <= 5; x++) {
-      int PlaceHand = choice.nextInt((5 - 1) + 1) + 1;
-      int PlaceField = choice.nextInt((5 - 1) + 1) + 1;
-      // Check if spot on field is empty, if it is not, just increment to next spot
-      if (ComputerOpponent.getPlayField() != null) {
-        x = x + 1;
-      }
-      // If spot empty just place the card on the field
-      else {
-        ComputerOpponent.placeCard(PlaceHand, PlaceField);
-      }
-    }
-  }
 
-  public void InvokeComputerAttack() {
-    Card[] playField = player.getPlayField();
-    Card[] compField = ComputerOpponent.getPlayField();
-    for (int x = 0; x <= 5; x++) {
-      //magicgame.magicthegathering. Computer attacks Player. i.e. Update player field.
-      try {
-        playField[x].health -= compField[x].attack;
-        compField[x].endTurn();
-      } catch (NullPointerException e) {
-        if (playField[x] == null) {
-          player.damageHealth(compField[x].attack);
-          compField[x].endTurn();
-          // else player doesn't have a card in current loc.
+ // while not loop place is null
+  public void computerPlaceCards() {
+    while (ComputerOpponent.getPlayField() != null){
+      for (int x = 0; x <= 5; x++) {
+        // iterate over playfield
+        int PlaceHand = choice.nextInt((5 - 1) + 1) + 1;
+        int PlaceField = choice.nextInt((5 - 1) + 1) + 1;
+        // Check if spot on field is empty, if it is not, just increment to next spot
+        if (ComputerOpponent.getPlayField() != null) {
+          x = x + 1;
+        }
+        // If spot empty just place the card on the field
+        else {
+          ComputerOpponent.placeCard(PlaceHand, PlaceField);
         }
       }
     }
